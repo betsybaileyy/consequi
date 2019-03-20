38071e231e814f129c2d392a6f8260fa31c326fb
t0ri <victoriarosemurray@gmail.com>
2019-03-20 11:12:57 -0700

    set up react router

---
+++ client/src/Routes/Routes.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+
+import Landing from './Landing/Landing.jsx'
+import GoalsIndex from './GoalsIndex/GoalsIndex.jsx'
+import GoalsNew from './GoalsNew/GoalsNew.jsx'
+import GoalsShow from './GoalsShow/GoalsShow.jsx'
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+
+        {/* "/" => GoalsIndex */}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            user ? (
+              <GoalsIndex {...this.props} />
+            ) : (
+              <Landing />
+            )
+          )} 
+        />
+
+        {/* "/new-goal" => GoalsNew */}
+        <Route
+          path="/new-goal"
+          render={() => (
+            user ? (
+              <GoalsNew {...this.props} />
+            ) : (
+              <Landing />
+            )
+          )}
+        />
+
+        {/* "/goal-show" => GoalsShow */}
+        <Route
+          path="/goal-show"
+          render={() => (
+            user ? (
+              <GoalsShow {...this.props} />
+            ) : (
+              <Landing />
+            )
+          )}
+        />
+      </div>
+    )
+  }
+}
