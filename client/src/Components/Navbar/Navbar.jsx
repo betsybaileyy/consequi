1380214322fed89dc1a113435eac6c787860fb65
t0ri <victoriarosemurray@gmail.com>
2019-03-21 16:31:55 -0700

    navbar styling complete

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Route, Link } from 'react-router-dom'
 
+import './navbar.css'
+
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <p>consequi</p>
-        <p>Hi, {this.props.user}! <Link to="/signout"><span id="sign-out">Sign out?</span></Link></p>
+      <div id="navbar-container">
+        <p id="navbar-container-logo-text">consequi</p>
+
+        <p id="navbar-container-account-text">
+          Hi, {this.props.user}!  &nbsp; 
+          <Link to="/signout">
+            <span id="navbar-container-sign-out-text">
+            Sign out?
+            </span>
+          </Link>
+        </p>
       </div>
     )
   }
