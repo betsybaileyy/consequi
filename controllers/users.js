f2e66bfe66337453def4dae1a3ea9b6f23825316
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-24 14:17:20 -0700

    cleaned up code

--- controllers/users.js
+++ controllers/users.js
@@ -3,63 +3,34 @@ const User = require('../models/user')
 
 
 module.exports = app => {
-        // showing a users profile
-        app.get('/user/:id', (req, res) => {
-            var currentUser = req.user;
-            console.log(req.cookies);
-            Goal.find().populate('author')
-                .then(goals => {
+
+    // SHOW a user's profile with all of their goals.
+    app.get("/user/:id", function(req, res) {
+        var currentUser = req.user;
+        if (currentUser) {
+            User.findById(currentUser)
+                .then(function(currentUser) {
+                    console.log(currentUser)
                     res.json({
-                        goals,
                         currentUser
-                    });
-                }).catch(err => {
-                    console.log(err.message);
-                })
-        })
-        // db.goals.find({
-        //     goals.author: req.params.id
-        // })
-
-        // SHOW list of favorites! Woo hoo
-        app.get('/user/favorites', function(req, res) {
-            const currentUser = req.user;
-            if (currentUser) {
-                User.findById(currentUser)
-                    .then(function(currentUser) {
-                        console.log(currentUser)
-                        res.render('list', {
-                            currentUser
-                        });
                     })
-            } else {
-                return res.status(401); // unauthorized
-            }
-        })
-
-
-        // SHOW a user's profile with all of their goals.
-        app.get("/user/:id", function(req, res) {
-            var currentUser = req.user;
-            if (currentUser) {
-                User.findById(currentUser)
-                    .then(function(currentUser) {
-                        console.log(currentUser)
-                        res.json({
-                            currentUser
-                        })
-                     })
-            // Goal.findById({
-            //         author: req.params.id
-            //     }).populate('tasks').lean()
-            //     .then(goal => {
-            //         res.json({
-            //             goal,
-            //             currentUser
-            //         });
-            //         // res.render("goals-show", { goal, currentUser });
-            //     })
-
+                })
         }
     })
 }
+
+
+// showing a users profile
+// app.get('/user/:id', (req, res) => {
+//     var currentUser = req.user;
+//     console.log(req.cookies);
+//     Goal.find().populate('author')
+//         .then(goals => {
+//             res.json({
+//                 goals,
+//                 currentUser
+//             });
+//         }).catch(err => {
+//             console.log(err.message);
+//         })
+// })
